9b180e2e567dbd39525d9622b27bb5afef024a0c
Mohsin Khan <mohsinkhan845@gmail.com>
2025-04-04 16:00:15 +0530

    Added a demo app to showcase capabilities of this library

---
+++ FormKitDemo/FormKit-Demo/FormKit-Demo/FormKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormKit_DemoApp.swift
+//  FormKit-Demo
+//
+//  Created by Mohsin Khan on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FormKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
